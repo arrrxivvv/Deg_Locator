ad93b687cd979c46c6f11f219ec2b2a9c8a77b7b
Hung-Hwa Lin <hunghwalin@gmail.com>
2022-11-03 22:46:26 -0700

    degDivRefine created

---
+++ src/degDivRefine.jl
@@ -0,0 +1,6 @@
+# using infiltrator
+
+function fNameAttrLstFunc( mSz, divLst, itNum, seed; fMod = "", dim = 3 )
+	attrLst, valLst = fAttrOptLstFunc( mSz, divLst, itNum, seed, dim );
+	fMain = ;
+end
