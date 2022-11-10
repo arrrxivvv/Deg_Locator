8e77e9dec4d50ab15d884b95b2dd66a98ae675ac
Hung-Hwa Lin <hunghwalin@gmail.com>
2022-11-10 11:20:12 -0800

    DegMatsOnGrid updated

--- src/DegMatsOnGrid.jl
+++ src/DegMatsOnGrid.jl
@@ -8,6 +8,15 @@ struct DegMatsOnGrid
 	HmatLst; # ::Array{Matrix{ComplexF64}};
 	HgridOpt::HgridStoreOpt;
 	eigWorkTh::ThrStruct{EigCustom.EigWork};
+	
+	eigenId::Base.RefValue{Int64};
+	eigenIdGrid::Array{Int64};
+	
+	function DegMatsOnGrid( params, Elst, vLst, HmatLst, HgridOpt, eigWorkTh )
+		eigenId = 0;
+		eigenIdGrid = zeros( Int64, params.divLst... );
+		new( params, Elst, vLst, HmatLst, HgridOpt, eigWorkTh, Ref(eigenId), eigenIdGrid );
+	end
 end
 
 function matsGridBase( params::DegParams, HmatLst, HOpt::HgridStoreOpt )
@@ -53,8 +62,26 @@ function getHLoc( loc::Vector{Int64}, matsGrid::DegMatsOnGrid )
 	end
 end
 
-function eigenAtLoc( loc::Vector{Int64}, matsGrid::DegMatsOnGrid )
+function startNextEigen( matsGrid::DegMatsOnGrid )
+	matsGrid.eigenId[] += 1;
+end
+
+function checkEigenDone( matsGrid::DegMatsOnGrid, loc::Vector{Int64} )
 	idLin = linIdFromIdVec( loc, matsGrid.params );
+	if matsGrid.eigenIdGrid[idLin] ==  matsGrid.eigenId[]
+		return true;
+	else
+		matsGrid.eigenIdGrid[idLin] = matsGrid.eigenId[];
+		return false;
+	end
+end
+
+function eigenAtLoc( loc::Vector{Int64}, matsGrid::DegMatsOnGrid; noReEigen = true )
+	idLin = linIdFromIdVec( loc, matsGrid.params );
+	if checkEigenDone( matsGrid, loc )
+		return;
+	end
+	
 	Hmat = getHLoc( loc, matsGrid );
 	if !isassigned( matsGrid.Elst, idLin )
 		matsGrid.Elst[idLin] = zeros(matsGrid.params.N);
