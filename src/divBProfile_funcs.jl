66a417b597671bcdf538e4afbe504930c9857a25
Hung-Hwa Lin <hunghwalin@gmail.com>
2022-11-17 18:24:10 -0800

    remove duplicate def of divB_profile_new

--- src/divBProfile_funcs.jl
+++ src/divBProfile_funcs.jl
@@ -29,41 +29,6 @@ function shLocLst( locLst, param_divide_num, dim, shId )
 	return locLst;
 end
 
-function divB_profile_new( mSz, divLst, itNum, seedFed; nDim = 3 )
-	if seedFed > 0
-		Random.seed!(seedFed);
-	end
-	minNum = 0;
-	maxNum = 2*pi;
-	paramsFull = degParamsInit( mSz, divLst, minNum, maxNum, nDim );
-	matsFull = matsGridHThreaded( paramsFull, threaded_zeros(ComplexF64,mSz,mSz) );
-	degBerrysFull = degBerrysInit( paramsFull, matsFull );
-	
-	totalNumLst = zeros(Int64,itNum);
-	
-	for it = 1 : itNum
-		print( "\r Iteration: " * string(it) * " / " * string(itNum) );
-		Hlst = DegLocatorDiv.HlstFunc(H_GUE,nDim,mSz);
-		HmatFun = (H,xLst) -> Hmat_3comb_ratio!( H, xLst, Hlst );
-		startNextEigen( matsFull );
-		
-		@info("Eigen:")
-		Utils.@timeInfo eigenAll( matsFull; HmatFun = HmatFun );
-
-		@info("Link:")
-		Utils.@timeInfo DegLocatorDiv.linksCalcAll( degBerrysFull );
-		
-		@info("Bfield:")
-		Utils.@timeInfo DegLocatorDiv.BfieldCalcAll( degBerrysFull );
-		
-		@info("DivB:")
-		Utils.@timeInfo DegLocatorDiv.divBCalcAll( degBerrysFull );
-		
-		totalNumLst[it] = sum(sum( (x->abs.(x).>1e-9).(degBerrysFull.divBLst) ));
-	end
-	return totalNumLst;
-end
-
 function divB_profile( param_dim = 2, Nlst=[20], num_it_main=2, param_divide=50, seedFedstart = -1, enumSaveMem = memNone; scale=degOptDefaultLst[1], ratioFed=degOptDefaultLst[2], fileNameMod = "", maxDepth = 0, param_divide_next = nothing, isOrtho = false, alpha = degOptDefaultLst[3], thresNM = rtFndDefaultLst[1], thresEDeg = rtFndDefaultLst[2], thresDegCollision = rtFndDefaultLst[2] )
 	ratio = ratioFed;
 	if ratioFed == degOptDefaultLst[2]
