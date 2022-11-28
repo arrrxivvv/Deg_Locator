32aefe9eea4e2817624ea3fad807256aa0fd80e2
Hung-Hwa Lin <hunghwalin@gmail.com>
2022-11-28 12:24:18 -0800

    run_rootfind_distill higher thresSzRatio

--- src/run_rootfind_distill_res.jl
+++ src/run_rootfind_distill_res.jl
@@ -3,13 +3,14 @@ using DegLocatorDiv
 nDim = 3;
 resLst = [5,10,15,20];
 # resLst = [5]
-mSzLst = [5,10,15,20];
-# mSzLst = [5];
+# mSzLst = [5,10,15,20];
+mSzLst = [10];
 itNum = 100;
 # itNum = 2;
 thresLst = [1e-9];
 thresValRatioLst = [10:10:100;];
-thresSzRatioLst = [10:10:100;];
+# thresSzRatioLst = [10:10:100;];
+thresSzRatioLst = [110:10:200;];
 divLst = ones(Int64, nDim);
 seed = 1000;
 
@@ -18,10 +19,15 @@ seed = 1000;
 for thres in thresLst 
 	for res in resLst
 		divLst .= res;
-		for mSz in mSzLst, thresValRatio in thresValRatioLst, thresSzRatio in thresSzRatioLst
-			print("mSz = $mSz, res = $res, thres = $thres, thresValRatio = $thresValRatio, thresSzRatio = $thresSzRatio", "               \r")
+		for mSz in mSzLst
+			for thresValRatio in thresValRatioLst
 			DegLocatorDiv.pick0gapLocsFromFile( mSz, divLst, itNum, seed; thresVal = thres, thresSz = thres, thresValRatio = thresValRatio );
-			DegLocatorDiv.locLstCollisionRemoveFromFile( mSz, divLst, itNum, seed; thresVal = thres, thresSz = thres, thresValRatio = thresValRatio, thresSzRatio = thresSzRatio );
+				for thresSzRatio in thresSzRatioLst
+				print("mSz = $mSz, res = $res, thres = $thres, thresValRatio = $thresValRatio, thresSzRatio = $thresSzRatio", "               \r")
+				
+				DegLocatorDiv.locLstCollisionRemoveFromFile( mSz, divLst, itNum, seed; thresVal = thres, thresSz = thres, thresValRatio = thresValRatio, thresSzRatio = thresSzRatio );
+				end
+			end
 		end
 	end
 end
