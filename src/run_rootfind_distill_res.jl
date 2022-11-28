bc306beb5a90d7b124ae789ef442c36ad7303fde
Hung-Hwa Lin <hunghwalin@gmail.com>
2022-11-27 21:21:10 -0800

    run_rootfind_distill updated

--- src/run_rootfind_distill_res.jl
+++ src/run_rootfind_distill_res.jl
@@ -1,12 +1,12 @@
 using DegLocatorDiv
 
-# resLst = [5,10,15,20];
-resLst = [5]
 nDim = 3;
-# mSzLst = [5,10,15,20];
-mSzLst = [5];
-# itNum = 100;
-itNum = 2;
+resLst = [5,10,15,20];
+# resLst = [5]
+mSzLst = [5,10,15,20];
+# mSzLst = [5];
+itNum = 100;
+# itNum = 2;
 thresLst = [1e-9];
 thresValRatioLst = [10:10:100;];
 thresSzRatioLst = [10:10:100;];
