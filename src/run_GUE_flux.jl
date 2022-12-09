1a773c135ed0acdf9a0aa42e51c62a22cef97072
Hung-Hwa Lin <hunghwalin@gmail.com>
2022-12-08 23:47:55 -0800

    run_GUE_flux res scales as WWE

--- src/run_GUE_flux.jl
+++ src/run_GUE_flux.jl
@@ -3,9 +3,21 @@ using Utils
 
 nlst = [10:10:60;];
 
-resLstBase = [80, 16, 16];
+divLstBase = [80, 16, 16];
+divLst = copy( divLstBase );
 
+resBase = 16;
+nBase = 10;
+
+itNum = 100;
+
+seed = 1000;
 
 for n in nlst
+	divLst .= Int64.( floor.( n ./ nBase .* divLstBase ) );
 	
+	println( "n = $n, res = $(divLst[end])" );
+	with_logger( errLogger )do 
+		@time divB_profile_flux( n, divLst, itNum, seed; enumSaveMem = memEig );
+	end
 end
