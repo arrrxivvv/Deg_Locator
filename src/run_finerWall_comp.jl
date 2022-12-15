a22e76eb3f4e95cc00da2d0872f52b93eb6ce85b
Hung-Hwa Lin <hunghwalin@gmail.com>
2022-12-15 12:41:22 -0800

    comparing finerWall results

--- src/run_finerWall_comp.jl
+++ src/run_finerWall_comp.jl
@@ -19,6 +19,7 @@ itNum = 10;
 seed = 1000;
 nDim = 3;
 dimRevLst = [nDim:-1:1;];
+ratFineLst = [2,4];
 
 fModMethod = "flux";
 fModMethodFiner = "fluxCell";
@@ -26,63 +27,89 @@ fMain = "deg_detailed";
 
 attrLst, valLst = fAttrOptLstFunc( mSz, divLst, itNum, seed; dim = nDim );
 
+attrMoreLst = ["ratFine"];
+
+attrLst, valLst = fAttrOptLstFunc( mSz, divLst, itNum, seed; dim = nDim );
 fName = fNameFunc( fMain, attrLst, valLst, jld2Type; fMod = fModMethod );
-fNameFiner = fNameFunc( fMain, attrLst, valLst, jld2Type; fMod = fModMethodFiner );
 
-divBLst1 = load( fName, "divBLstLst" );
-divBLstF = load( fNameFiner, "divBLstLst" );
+attrLst, valLst = fAttrOptLstFunc( mSz, divLst, itNum, seed; dim = nDim, attrMoreLst = attrMoreLst, valMoreLst = [ratFineLst[1]] );
+fNameFiner2 = fNameFunc( fMain, attrLst, valLst, jld2Type; fMod = fModMethodFiner );
 
-NLstPol1 = load( fName, "NLstPol" );
-NLstPolF = load( fNameFiner, "NLstPol" );
+attrLst, valLst = fAttrOptLstFunc( mSz, divLst, itNum, seed; dim = nDim, attrMoreLst = attrMoreLst, valMoreLst = [ratFineLst[2]] );
+fNameFiner4 = fNameFunc( fMain, attrLst, valLst, jld2Type; fMod = fModMethodFiner );
 
-locLstPol1 = load( fName, "locLstPol" );
+fNameLst = [fName, fNameFiner2, fNameFiner4];
 
-posLst = CartesianIndices( divBLst1[1] );
+divBLsts = [];
+BfieldLsts = [];
+NLstPols = [];
+locLstPols = [];
 
-BfieldLst1 = load( fName, "BfieldLstLst" );
-BfieldLstF = load( fNameFiner, "BfieldLstLst" );
-
-dimOffset = 2;
-
-BfieldArr1 = [
-	BfieldLst1[it][iDim][pos][n]
-	for iDim = 1 : nDim, n = 1:mSz, pos in posLst, it = 1:itNum];
-BfieldArrF = [
-	BfieldLstF[it][iDim][pos][n]
-	for iDim = 1 : nDim, n = 1:mSz, pos in posLst, it = 1:itNum];
-	
-BfieldLstCell1 = makeArrCell( BfieldArr1, nDim, dimOffset, dimRevLst, posLst, itNum, mSz );
-BfieldLstCellF = makeArrCell( BfieldArrF, nDim, dimOffset, dimRevLst, posLst, itNum, mSz );
-
-divBLstDiff = zeros( mSz, divLst..., itNum );
-for it = 1 : itNum
-	for pos in posLst
-		@view( divBLstDiff[:,pos,it] ) .= real.( divBLstF[it][pos] .- divBLst1[it][pos] );
-	end
+for iF = 1 : length(fNameLst)
+	push!( divBLsts, load( fNameLst[iF], "divBLstLst" ) );
+	push!( BfieldLsts, load( fNameLst[iF], "BfieldLstLst" ) );
+	push!( NLstPols, load( fNameLst[iF], "NLstPol" ) );
+	push!( locLstPols, load( fNameLst[iF], "locLstPol" ) );
 end
 
-BfieldLstDiff = zeros( nDim, mSz, divLst..., itNum );
-BfieldLstDiff .= real.( BfieldArrF .- BfieldArr1 );
-# for it = 1:itNum, iDim = 1:nDim
+dimOffset = 2;
+posLst = CartesianIndices( divBLsts[1] );
+
+BfieldArrs = [];
+BfieldArrCells = [];
+divBAllLevels = [];
+for iF = 1 : length(fNameLst)
+	push!( divBAllLevels, (x->real.(sum.(x))).(divBLsts[iF]) );
+	push!( BfieldArrs, [
+	BfieldLsts[iF][it][iDim][pos][n]
+	for iDim = 1 : nDim, n = 1:mSz, pos in posLst, it = 1:itNum] );
+	push!( BfieldArrCells, makeArrCell( BfieldArrs[iF], nDim, dimOffset, dimRevLst, posLst, itNum, mSz ) );
+end
+
+# divBLst1 = load( fName, "divBLstLst" );
+# divBLstF = load( fNameFiner, "divBLstLst" );
+
+# NLstPol1 = load( fName, "NLstPol" );
+# NLstPolF = load( fNameFiner, "NLstPol" );
+
+# locLstPol1 = load( fName, "locLstPol" );
+
+# posLst = CartesianIndices( divBLst1[1] );
+
+# BfieldLst1 = load( fName, "BfieldLstLst" );
+# BfieldLstF = load( fNameFiner, "BfieldLstLst" );
+
+# dimOffset = 2;
+
+# BfieldArr1 = [
+	# BfieldLst1[it][iDim][pos][n]
+	# for iDim = 1 : nDim, n = 1:mSz, pos in posLst, it = 1:itNum];
+# BfieldArrF = [
+	# BfieldLstF[it][iDim][pos][n]
+	# for iDim = 1 : nDim, n = 1:mSz, pos in posLst, it = 1:itNum];
+	
+# BfieldLstCell1 = makeArrCell( BfieldArr1, nDim, dimOffset, dimRevLst, posLst, itNum, mSz );
+# BfieldLstCellF = makeArrCell( BfieldArrF, nDim, dimOffset, dimRevLst, posLst, itNum, mSz );
+
+# divBLstDiff = zeros( mSz, divLst..., itNum );
+# for it = 1 : itNum
 	# for pos in posLst
-		# @view( BfieldLstDiff[iDim,:, pos, it] ) .= real.( BfieldLstF[it][iDim][pos] .- BfieldLst1[it][iDim][pos] );
+		# @view( divBLstDiff[:,pos,it] ) .= real.( divBLstF[it][pos] .- divBLst1[it][pos] );
 	# end
 # end
 
-BfieldLstDiffShLst = [ ShiftedArrays.circshift( BfieldLstDiff, ntuple( x-> x==2+iDim ? -1 : 0, 6 ) ) for iDim = 1 : nDim ];
+# BfieldLstDiff = zeros( nDim, mSz, divLst..., itNum );
+# BfieldLstDiff .= real.( BfieldArrF .- BfieldArr1 );
 
-# BfieldLstDiffCell = [
-	# iBnd == 1 ? - BfieldLstDiff[iDim,n,pos,it] : BfieldLstDiffShLst[dimRevLst[iDim]][iDim,n,pos,it]
-	# for iDim = 1:nDim, iBnd = 1:2, n = 1:mSz, pos in posLst, it =1:itNum];
-BfieldLstDiffCell = makeArrCell( BfieldLstDiff, nDim, dimOffset, dimRevLst, posLst, itNum, mSz );
+# BfieldLstDiffShLst = [ ShiftedArrays.circshift( BfieldLstDiff, ntuple( x-> x==2+iDim ? -1 : 0, 6 ) ) for iDim = 1 : nDim ];
 
-thres = 1e-6;
+# thres = 1e-6;
 
-idSameLst = [
-	findall( x->abs(x)<1e-6, @view( divBLstDiff[n, :,:,:, it] ) )
-	for n = 1:mSz, it = 1:itNum];
+# idSameLst = [
+	# findall( x->abs(x)<1e-6, @view( divBLstDiff[n, :,:,:, it] ) )
+	# for n = 1:mSz, it = 1:itNum];
 	
-BsameLst = [[
-	iBnd == 1 ? - BfieldLstDiff[iDim, n, idSameLst[n,it][iLoc], it] : BfieldLstDiffShLst[dimRevLst[iDim]][iDim, n, idSameLst[n,it][iLoc], it]
-	for iLoc = 1 : length(idSameLst[n, it]), iDim = 1:nDim, iBnd = 1:2 ]
-	for n = 1:mSz, it = 1:itNum];
+# BsameLst = [[
+	# iBnd == 1 ? - BfieldLstDiff[iDim, n, idSameLst[n,it][iLoc], it] : BfieldLstDiffShLst[dimRevLst[iDim]][iDim, n, idSameLst[n,it][iLoc], it]
+	# for iLoc = 1 : length(idSameLst[n, it]), iDim = 1:nDim, iBnd = 1:2 ]
+	# for n = 1:mSz, it = 1:itNum];
